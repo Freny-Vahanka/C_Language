068ad4762cc6847573a193e067340c983c22d385
Freny-Vahanka <fvahanka@gmail.com>
2023-04-27 12:34:30 +0530

    CH1 - Introduction to C-Language

---
+++ CH1/F-1-2.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{	clrscr();
+	printf("\t\tRESUME\nName:Freny Vahanka\n");
+	printf("Gender:Female\n");
+	printf("Qualifications:12th pass\n\n");
+	printf("Qualities:\n\t1)Creative\n\t2)Hardworking\n\t3)Obidient\n\n");
+	printf("Hobbies: Art and Music\n\n");
+	printf("Extra curricular:\n\t1)Interschool Band Compitetion\n\t2)Interschool Drawing Competition\n\t3)Helped at a cause for social service\n\n");
+	printf("School:The Millennium School Surat\n\n");
+	printf("Contact no:7046584933\n");
+	printf("Email-id:fvahanka@gmail.com");
+	getch();
+
+
+}
